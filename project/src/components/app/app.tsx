fd56c91ab785464ca1e6b5eb3795da59febb4a1d
gryda <gryda@hismith.ru>
2022-07-12 13:13:42 +0300

    ls2

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+// import Main from '../pages/main/main';
+import ResultSuccess from '../pages/result-success/result-success';
+
+type AppScreenProps = {
+  errorsCount: number
+}
+
+function App({errorsCount}: AppScreenProps): JSX.Element {
+  // return <Main errorsCount={errorsCount}/>;
+  return <ResultSuccess/>;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const SETTINGS = {
+  ERRORS_COUNT: 3,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App errorsCount={SETTINGS.ERRORS_COUNT}/>
   </React.StrictMode>,
   document.getElementById('root'));
